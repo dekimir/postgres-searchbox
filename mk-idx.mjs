c7d3ee84d70588c9eb38aeac3a220d18a1c8cc59
Dejan Mircevski <github@mircevski.com>
2022-12-08 17:55:17 -0500

    mk-idx: consume psql output; handle space in names
    Make mk-idx work directly off psql output, so we can pipe it in.
    
    Also put double quotes around table/column/index names, to ensure all valid identifiers are handled correctly.

--- mk-idx.mjs
+++ mk-idx.mjs
@@ -8,18 +8,20 @@ var table = 'your_table_name_here'
 var textColumns = []
 
 for await (const line of rl) {
-  const m = line.match(/\\d\s+"?(\w+)"?/)
-  if (m !== null) { table = m[1] }
+  const m = line.match(/^\s*Table "(.*)"$/)
+  if (m !== null) {
+    table = m[1].replace('public.', '')
+  }
   else {
     const parts = line.split('|')
-    if (parts.length > 2 && parts[2].trim() === 'text') {
-      textColumns.push(parts[1].trim())
+    if (parts.length > 1 && parts[1].trim() === 'text') {
+      textColumns.push(parts[0].trim())
     }
   }
 }
 
-const valExpr = textColumns.map(c => `COALESCE(${c}, '') || ''`).join(' || ')
+const valExpr = textColumns.map(c => `COALESCE("${c}", '') || ''`).join(' || ')
 
 console.log(
-  `ALTER TABLE ${table} ADD COLUMN postgres_searchbox_v1_doc tsvector GENERATED ALWAYS AS (to_tsvector('english', ${valExpr})) STORED;`)
-console.log(`CREATE INDEX postgres_searchbox_v1_idx_${table} ON ${table} USING GIN(postgres_searchbox_v1_doc);`)
+  `ALTER TABLE "${table}" ADD COLUMN postgres_searchbox_v1_doc tsvector GENERATED ALWAYS AS (to_tsvector('english', ${valExpr})) STORED;`)
+console.log(`CREATE INDEX "postgres_searchbox_v1_idx_${table}" ON "${table}" USING GIN(postgres_searchbox_v1_doc);`)
