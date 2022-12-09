e71571f7d2ac24abceac93fda22fbd72fcb7e53b
Dejan Mircevski <github@mircevski.com>
2022-12-09 01:32:25 -0500

    Fix import/export problems after publishing
    These problems didn't show up in local development, but of course they
    promptly appeared upon publication, when the package is installed from
    npm. :(

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { Client } from 'pg'
-import * as sql_format from 'pg-format'
+import pkg from 'pg';
+const { Client } = pkg;
+import format from 'pg-format'
 
 const client = new Client()
 client.connect()
@@ -8,7 +9,7 @@ export async function handlerNextJS(req, res) {
   const json = JSON.parse(req.body)
   const query = json.params.query
   const table = json.indexName
-  const sql = sql_format('SELECT * FROM %I WHERE postgres_searchbox_v1_doc @@ websearch_to_tsquery(%L) LIMIT 10', table, query)
+  const sql = format('SELECT * FROM %I WHERE postgres_searchbox_v1_doc @@ websearch_to_tsquery(%L) LIMIT 10', table, query)
   const matches = await client.query(sql)
   res.status(200).json({
     results: [ { hits: matches.rows } ],
